fc247b50a7af8364cb5e815f80d748e5084a7bdf
Lukas Reimer <lukas.reimer@icloud.com>
2018-03-06 11:18:44 +0100

    added more rudimentary views and routing for polls app

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -6,5 +6,12 @@ from . import views
 
 
 urlpatterns = [
-    path(route="", view=views.index, name="index")
+    # e.g. /polls/
+    path(route="", view=views.index, name="index"),
+    # e.g. /polls/5/
+    path(route="<int:question_id>/", view=views.detail, name="detail"),
+    # e.g. /polls/5/results/
+    path(route="<int:question_id>/results/", view=views.results, name="results"),
+    # e.g. /polls/5/vote/
+    path(route="<int:question_id>/vote/", view=views.vote, name="vote"),
 ]
