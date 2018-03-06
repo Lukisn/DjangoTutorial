8b838f2cbbee1d608c043d6a54623e113c739c7f
Lukas Reimer <lukas.reimer@icloud.com>
2018-03-06 11:43:16 +0100

    added shortcut to details view implementation of polls app

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.template import loader
 from django.http import HttpResponse, Http404
 
@@ -14,10 +14,11 @@ def index(request):
 
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist!")
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist!")
+    question = get_object_or_404(Question, pk=question_id)
     context = {
         "question": question
     }
