01ea555ed3f23f7e82decfcb0acf8c4b72057849
Lukas Reimer <lukas.reimer@icloud.com>
2018-03-06 13:36:34 +0100

    added generic views

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -7,12 +7,8 @@ from . import views
 
 app_name = "polls"
 urlpatterns = [
-    # e.g. /polls/
-    path(route="", view=views.index, name="index"),
-    # e.g. /polls/5/
-    path(route="<int:question_id>/", view=views.detail, name="detail"),
-    # e.g. /polls/5/results/
-    path(route="<int:question_id>/results/", view=views.results, name="results"),
-    # e.g. /polls/5/vote/
+    path(route="", view=views.IndexView.as_view(), name="index"),
+    path(route="<int:pk>/", view=views.DetailView.as_view(), name="detail"),
+    path(route="<int:pk>/results/", view=views.ResultsView.as_view(), name="results"),
     path(route="<int:question_id>/vote/", view=views.vote, name="vote"),
 ]
