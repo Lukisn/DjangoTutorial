fc19a3631601d7f27cb5540e3cca485e5fd51e62
Lukas Reimer <lukas.reimer@icloud.com>
2018-03-06 11:40:34 +0100

    implemented detail view of polls app

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.template import loader
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 
 from .models import Question
 
@@ -14,7 +14,14 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse(f"You're looking at question {question_id}")
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist!")
+    context = {
+        "question": question
+    }
+    return render(request, "polls/detail.html", context)
 
 
 def results(request, question_id):
