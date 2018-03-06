c7ada40c7ad3f5a8d5371df2cc3c9db3b54c6a1d
Lukas Reimer <lukas.reimer@icloud.com>
2018-03-06 15:20:36 +0100

    added inline choices to admin interface for Question model

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -3,5 +3,20 @@ from django.contrib import admin
 from .models import Question, Choice
 
 
-admin.site.register(Question)
-admin.site.register(Choice)
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 1
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    # fields = ["pub_date", "question_text"]
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"],
+                              "classes": ["collapse"]}),
+    ]
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
+# admin.site.register(Choice)
