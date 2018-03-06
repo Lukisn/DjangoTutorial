5f347f759152a4a8da8c35e00d46a1f7390de73a
Lukas Reimer <lukas.reimer@icloud.com>
2018-03-06 13:51:01 +0100

    added more tests for Question model

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import timedelta
 
 from django.utils import timezone
 from django.test import TestCase
@@ -9,6 +9,19 @@ from .models import Question
 class QuestionModelTests(TestCase):
 
     def test_was_published_recently_with_future_question(self):
-        time = timezone.now() + datetime.timedelta(days=30)
+        """Test function returns False on future question."""
+        time = timezone.now() + timedelta(days=30)
         future_question = Question(pub_date=time)
         self.assertFalse(future_question.was_published_recently())
+
+    def test_was_published_recently_with_old_question(self):
+        """Test function returns False on question older than 1 day."""
+        time = timezone.now() - timedelta(days=1, seconds=1)
+        old_question = Question(pub_date=time)
+        self.assertFalse(old_question.was_published_recently())
+
+    def test_was_published_recently_with_recent_question(self):
+        """Test function returns True on question from within the last day."""
+        time = timezone.now() - timedelta(hours=23, minutes=59, seconds=59)
+        recent_question = Question(pub_date=time)
+        self.assertTrue(recent_question.was_published_recently())
