8ff690aea30856cf134732b5aa4075cd933873e9
Lukas Reimer <lukas.reimer@icloud.com>
2018-03-06 15:23:53 +0100

    changed inline choices to use tabular class

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -3,13 +3,12 @@ from django.contrib import admin
 from .models import Question, Choice
 
 
-class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 1
 
 
 class QuestionAdmin(admin.ModelAdmin):
-    # fields = ["pub_date", "question_text"]
     fieldsets = [
         (None, {"fields": ["question_text"]}),
         ("Date information", {"fields": ["pub_date"],
@@ -19,4 +18,3 @@ class QuestionAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Question, QuestionAdmin)
-# admin.site.register(Choice)
