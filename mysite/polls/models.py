dc83ca595240f4e7897cb2101a5c621fc7f767a6
Lukas Reimer <lukas.reimer@icloud.com>
2018-03-06 10:26:51 +0100

    renamed pub_date column of the Question model

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pud_date = models.DateTimeField("publishing date")
+    pub_date = models.DateTimeField("publishing date")
 
 
 class Choice(models.Model):
