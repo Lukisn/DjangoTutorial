a2ab85be63d652fdbe2cc79d66f3b836469a677e
Lukas Reimer <lukas.reimer@icloud.com>
2018-04-11 10:09:52 +0200

    tweaked admin interface and books app views and templates

--- mybooksite/books/admin.py
+++ mybooksite/books/admin.py
@@ -1,8 +1,35 @@
 from django.contrib import admin
 from .models import Publisher, Author, Book
-
-
-admin.site.register(Publisher)
-admin.site.register(Author)
-admin.site.register(Book)
 # iniital superuser: admin, ihavecowpowers
+
+
+class PublisherAdmin(admin.ModelAdmin):
+    list_display = ("name", "city", "state_province", "country", "website")
+    list_filter = ("country", "state_province", "city")
+    search_fields = ("name", )
+
+
+admin.site.register(Publisher, PublisherAdmin)
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("full_name", "first_name", "last_name", "email")
+    search_fields = ("first_name", "last_name")
+
+
+admin.site.register(Author, AuthorAdmin)
+
+
+class BookAdmin(admin.ModelAdmin):
+    # admin form customization:
+    # fields = []  # entry field ordering
+    # fieldsets = [(, {"fields": []})]  # entry field grouping
+    # inlines = []  # inline model entries
+    # change list customization:
+    list_display = ("title", "publisher", "publication_date")
+    list_filter = ("publisher", "publication_date")
+    search_fields = ("title", )
+
+
+admin.site.register(Book, BookAdmin)
+
--- mybooksite/books/views.py
+++ mybooksite/books/views.py
@@ -8,7 +8,7 @@ def index(request):
     book = choice(Book.objects.all())
     author = choice(Author.objects.all())
     publisher = choice(Publisher.objects.all())
-    return render(request, "books_index.html", {
+    return render(request, "books/index.html", {
         "book": book,
         "author": author,
         "publisher": publisher,
@@ -17,29 +17,33 @@ def index(request):
 
 def list_authors(request):
     authors = Author.objects.all()
-    return render(request, "list_authors.html", {"authors": authors})
+    return render(request, "books/list_authors.html", {"authors": authors})
 
 
 def list_books(request):
     books = Book.objects.all()
-    return render(request, "list_books.html", {"books": books})
+    return render(request, "books/list_books.html", {"books": books})
 
 
 def list_publishers(request):
     publishers = Publisher.objects.all()
-    return render(request, "list_publishers.html", {"publishers": publishers})
+    return render(request, "books/list_publishers.html", {
+        "publishers": publishers
+    })
 
 
 def author_details(request, author_id):
     author = Author.objects.get(id=author_id)
-    return render(request, "author_details.html", {"author": author})
+    return render(request, "books/details_author.html", {"author": author})
 
 
 def book_details(request, book_id):
     book = Book.objects.get(id=book_id)
-    return render(request, "book_details.html", {})
+    return render(request, "books/details_book.html", {"book": book})
 
 
 def publisher_details(request, publisher_id):
     publisher = Publisher.objects.get(id=publisher_id)
-    return render(request, "publisher_details.html", {})
+    return render(request, "books/details_publisher.html", {
+        "publisher": publisher
+    })
